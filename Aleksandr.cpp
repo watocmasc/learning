215425b4b4074c39706ea765eef8b2ccc362c2af
keyboard <oushen.ms@gmail.com>
2024-07-22 10:37:27 +0200

    update

--- Aleksandr.cpp
+++ Aleksandr.cpp
@@ -1,60 +1,51 @@
 #include <iostream>
 #include <typeinfo>
+
 using namespace std;
+
  int main()
  {
- 	int x=4,y=4,x2,y2,f=0;
+ 	
+ 	int x = 4, y = 4, x2, y2, f = 0;
     char symbol='y'; 
-    //cin>>symbol;
+    
+    // cin>>symbol;
+    
     while(1)
     {
-    	cout<<"write position of King"<<endl;
-    	cin>>x2>>y2;
-    if(typeid(x2).name()!=typeid(2).name())
-    break;
-	
-			
+    	cout << "write position of King" << endl;
+    	cin >> x2 >> y2;
 		
-		 if((x-x2==1 || x-x2==-1) && y2==y)
+		// проверка на верт. и гориз. ход
+		if (
+			( (y-y2==1 || y-y2==-1) && x2==x ) ||
+			( (x-x2==1 || x-x2==-1) && y2==y )
+		   )
     	{
-    		x=x2;
+    		x = x2;
+    		y = y2;
 		}
-		else if((y-y2==1 || y-y2==-1) && x2==x)
-    	{
-    		y=y2;
-		}
-		else if ((x-x2==1) && (y-y2==1))
+		// проверка на диагональный ход
+		else if ( (x-x2 == 1 || x-x2 == -1) && (y-y2==1 || y-y2==-1) )
 		{
 			x=x2;
 			y=y2;
 		}
-			else if ((x-x2==-1) && (y-y2==-1))
-		{
-			x=x2;
-			y=y2;
-		}
-			else if ((x-x2==-1) && (y-y2==1))
-		{
-			x=x2;
-			y=y2;
-		}
-			else if ((x-x2==1) && (y-y2==-1))
-		{
-			x=x2;
-			y=y2;
-		}
-	
 		else{
-			cout<<"incorect"<<endl;
-			 f++;
+			// если произошла ошибка, более 3 раз
+			cout << "incorect" << endl;
+			f++;
 			if(f==3)
 			{
-			cout<<"do you want play again again?"<<endl;
-			cin>>symbol;
-			if(symbol!='y')
-			{
-			break;	
-			}
+				cout << "do you want play again again?" << endl;
+				
+				cin >> symbol;
+				
+				// вопрос о продолжении игры
+				if( symbol != 'y' )
+				{
+						break;	
+				}
 			} 
 			
 			
