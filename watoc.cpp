d4d096b8dc49d0e4267c382fe50cbf7e8aa9c48f
keyboard <oushen.ms@gmail.com>
2024-07-22 09:37:56 +0200

    update

---
+++ watoc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct info {
+	int phone;
+	string name;
+} user;
+
+int main()
+{	
+	
+	user.phone = 10;
+	user.name = "Man";
+	
+	cout << user.name;
+	
+	
+	
+	
+}
